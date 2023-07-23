8214c96f9c5daf8fd1918f1e1dd98e7312b72551
chabiaknt <chabiaknt@gmail.com>
2023-07-23 21:03:33 +0530

    share added

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,38 @@
+import "./share.css";
+import { PermMedia, Label, Room, EmojiEmotions } from "@mui/icons-material";
+
+export default function Share() {
+  return (
+    <div className="share">
+        <div className="shareWrapper">
+            <div className="shareTop">
+                <img src="/assets/person/1.jpeg.JPG" alt="photos" className="shareProfileImg" />
+                <input className="shareInput" placeholder="Whats in your mind....." />
+            </div>
+            <hr className="shareHr" />
+            <div className="shareBottom">
+                <div className="shareOptions">
+                    <div className="shareOption">
+                        <PermMedia htmlColor="tomato" className="shareIcon"/>
+                        <span className="shareOptionText">Photos or videos</span>
+                    </div>
+                    <div className="shareOption">
+                        <Label htmlColor="blue" className="shareIcon"/>
+                        <span className="shareOptionText">Tags</span>
+                    </div>
+                    <div className="shareOption">
+                        <Room htmlColor="green" className="shareIcon"/>
+                        <span className="shareOptionText">Location</span>
+                    </div>
+                    <div className="shareOption">
+                        <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
+                        <span className="shareOptionText">Feelings</span>
+                    </div>
+                </div>
+                <button className="shareButton">Share</button>
+            </div>
+        </div>
+    </div>
+
+  )
+}
