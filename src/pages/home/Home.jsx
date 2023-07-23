ad7937899b024ba92404627bf074bc893be08415
chabiaknt <chabiaknt@gmail.com>
2023-07-23 19:39:28 +0530

    second commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Topbar from "../../components/topbar/Topbar";
+import "./Home.css"
+
+export default function Home() {
+  return (
+    <>
+      <Topbar/>
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  )
+}
