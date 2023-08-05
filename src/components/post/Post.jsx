dbb44d0dcaff06a685cdf6d761e1e1e6a360f2df
chabiaknt <chabiaknt@gmail.com>
2023-08-05 14:10:53 +0530

    like functionality

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './post.css';
 import { MoreVert } from '@mui/icons-material';
 import {Users} from '../../dummyData';
 
 export default function Post({post}) {
-    
+
+    const [like,setLike] = useState(post.like);
+    const [isLiked,setIsLiked] = useState(false);
+
+    const likeHandler = ()=>{
+        setLike(isLiked ? like-1 : like+1)
+        setIsLiked(!isLiked)
+    }
+
   return (
     <div className='post'>
         <div className="postWrapper">
@@ -24,9 +32,9 @@ export default function Post({post}) {
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                    <img className='likeIcon' src="/assets/like.png" alt="" />
-                    <img className='heartIcon' src="/assets/heart.png" alt="" />
-                    <span className="postLikeCounter">{post.like} people like</span>
+                    <img className='likeIcon' src="/assets/like.png" onClick={likeHandler} alt="" />
+                    <img className='likeIcon' src="/assets/heart.png" onClick={likeHandler} alt="" />
+                    <span className="postLikeCounter">{like} people like</span>
                 </div>
                 <div className="postBottomRight">
                     <span className="postCommentText">{post.comment} comments</span>
